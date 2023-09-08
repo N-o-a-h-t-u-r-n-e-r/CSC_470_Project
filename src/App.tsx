0fec441885ac635dd5b0d16c43063176384c8593
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-09-08 15:34:40 +0000

    data base working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import React from "react";
-import "./styles.css";
+import React, { useRef } from "react";
+import { addDoc, collection } from "@firebase/firestore"
+import { firestore } from "./firebase_setup/firebase"
 
 const App = () => {
+  const dataRef = useRef<HTMLInputElement>(null);
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+  
+    const ref = collection(firestore, "test") 
+    
+    try {
+        addDoc(ref, {data: dataRef.current!.value})
+    } catch(err) {
+        console.log(err)
+    }
+  
+    dataRef.current!.value = "";
+  }
+
   return (
     <div>
       <p>Your cleaned App component</p>
+      <form onSubmit={handleSubmit}>
+        <input type= "text" ref={dataRef} />
+        <button type = "submit">Save</button>
+      </form>
     </div>
+    
   );
 };
 
