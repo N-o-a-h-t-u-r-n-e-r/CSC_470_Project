8b212b4a74c707f6925d85eee615605d7dd2f1ca
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-09-08 17:49:11 +0000

    firebase hosting

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import "./styles.css";
 import App from "./App";
@@ -8,5 +9,17 @@ const container = document.getElementById('app');
 const root = createRoot(container!); 
 
 root.render(
-    <App />
+    <Router>
+        <Routes>
+            <Route path="/about" element={
+                <div>
+                    <h1>About Us</h1>
+                    <p>This is the about page content.</p>
+                </div>
+            } />
+            <Route path="/" element={
+                <App />
+            } />
+        </Routes>
+    </Router>
 );
