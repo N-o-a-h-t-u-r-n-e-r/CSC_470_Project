24a41099c642f35c589970c111726c8277c6c73a
Jake Shawd <90359526+Shewwd@users.noreply.github.com>
2023-09-10 00:39:39 +0000

    remove

--- src/index.tsx
+++ src/index.tsx
@@ -12,19 +12,6 @@ const root = createRoot(container!);
 root.render(
     <Router>
         <Routes>
-            <Route path="/liv" element={
-                <div className="app-container">
-                    <div className="app-header">
-                    </div>
-                    <div className="app-content">
-                        <h1>Hello Liv</h1>
-                        <h1 style={{color: "red"}}>I Love you</h1>
-                        <h1 style={{color: "red"}}>&#60;3</h1>
-                    </div>
-                    <div className="app-footer">
-                    </div>
-                </div>
-            }/>
             <Route path="/" element={
                 <App />
             }/>
