e1f76b13c3eb9b8e15c30e88a80e2847a23b1071
GageN48 <77462929+GageN48@users.noreply.github.com>
2023-11-15 23:08:15 +0000

    working search bar no output

--- src/Pages/Shared/ExerciseSearch.tsx
+++ src/Pages/Shared/ExerciseSearch.tsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import { firestore } from "../../firebase_setup/firebase";
+import { getDocs, collection } from "@firebase/firestore"
+import ExerciseManager from "../../Managers/ExerciseManager";
+import { setUncaughtExceptionCaptureCallback } from "process";
 
 interface Props {
 trigger:boolean;
@@ -6,11 +10,20 @@ setTrigger: (value: boolean) => void;
 }
 
 const ExerciseSearch = (props: Props) => {
+    
+    const [input, setInput] = useState("");
+    const [results, setResults] = useState("");
 
     return(props.trigger) ? (
         <div className="exercise-search-popup-box">
             <div className="exercise-search-box">    
                 <button className="exercise-search-button" onClick={() => props.setTrigger(false)}> X </button>
+                <div className="input-wrapper">
+                    <input placeholder="Type to search..." 
+                    value = {input}
+                    onChange={(e) => setInput(e.target.value)}
+                    />
+                </div>
             </div>
         </div>
     ): null;
