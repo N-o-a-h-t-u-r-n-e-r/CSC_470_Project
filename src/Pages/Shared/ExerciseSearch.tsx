84b38524b1136eca77d4b3859c2767798a7f3b2a
GageN48 <77462929+GageN48@users.noreply.github.com>
2023-11-15 16:09:16 +0000

    added popup to add exercise button

--- src/Pages/Shared/ExerciseSearch.tsx
+++ src/Pages/Shared/ExerciseSearch.tsx
@@ -1,14 +1,19 @@
-interface Props {
+import React, { useState } from "react";
 
+interface Props {
+trigger:boolean;
+setTrigger: (value: boolean) => void;
 }
 
 const ExerciseSearch = (props: Props) => {
 
-    return(
-        <>
-            <p>test </p>
-        </>
-    );
+    return(props.trigger) ? (
+        <div className="exercise-search-popup-box">
+            <div className="exercise-search-box">    
+                <button className="exercise-search-button" onClick={() => props.setTrigger(false)}> X </button>
+            </div>
+        </div>
+    ): null;
 
 }
 
