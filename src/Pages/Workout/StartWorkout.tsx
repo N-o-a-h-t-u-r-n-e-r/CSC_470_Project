0582a29bd86552cc47bff2205ef3cecd2f94ec36
N-o-a-h-t-u-r-n-e-r <77465733+N-o-a-h-t-u-r-n-e-r@users.noreply.github.com>
2023-11-16 01:30:59 +0000

    start workout

--- src/Pages/Workout/StartWorkout.tsx
+++ src/Pages/Workout/StartWorkout.tsx
@@ -7,13 +7,13 @@ const StartWorkout = (props: Props) => {
     return(
         <div className="start-workout-container">
             <div className="start-workout-header">
-            
+                <h1>Start Workout</h1>
             </div>
             <div className="start-workout-body">
-            
+                <h2>Presets:</h2>
             </div>
             <div className="start-blank-workout-button">
-                
+                <button className='nav-buttons'><h3>Start Blank Workout</h3></button>
             </div>
         </div>
     );
